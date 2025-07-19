32906f8ea2bf2515c75f69746cd738db75927474
Akomon333 <akomon10@gmail.com>
2025-07-19 13:10:51 +0300

    Two Sum test

--- main.cpp
+++ main.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
-#include<vector>
+#include <vector>
+
+#include <map>
+#include <unordered_map>
+
 
 
 int main(){
    
-    int e;
-    e = 10;
+    class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        std::unordered_map<int,int> PossibleSolutions;
+        std::pair<int,int> SolutionForTask;
+        for (int i = 0; i < nums.size(); i++){
+            int CurrentNum = nums[i];
+            int Diffirence = target - CurrentNum;
+
+            if(PossibleSolutions.find(Diffirence) != PossibleSolutions.end()){
+                SolutionForTask.first = PossibleSolutions[Diffirence];
+                SolutionForTask.second = i;
+                break;
+            } else{
+                PossibleSolutions[CurrentNum] = i;
+            }
+        }
+        
+        return {SolutionForTask.first,SolutionForTask.second};
+    }
+};
 
     return 0;
 }
